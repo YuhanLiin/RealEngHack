79e7a69618e71dc4c384d660671e9240a315fbc0
yuhan lin <yuhan lin>
2017-05-27 14:12:21 -0400

    Integrated bg and sprites

--- js/bundle.js
+++ js/bundle.js
@@ -196,14 +196,22 @@ function drawGame(game){
     //Draw other shit
 }
 
+var bg = document.getElementById('bg');
 function eraseGame(game){
-    ctx.clearRect(0,0,canvas.width, canvas.height);
+    ctx.drawImage(bg, 0, 0, canvas.width, canvas.height);
 }
 
-function drawPlayer(player){
-    ctx.fillStyle = "Blue";
-    ctx.fillRect(player.x-player.width/2, player.y-player.height/2, player.width, player.height);
-}
+var playerSprites = [document.getElementById('player1'), document.getElementById('player2')];
+var drawPlayer = function(){
+    var iteration = 0;
+    var duration = 10
+    return function (player){
+        ctx.drawImage(playerSprites[Math.floor(iteration/duration)], player.x-player.width/2, 
+            player.y-player.height/2, player.width, player.height);
+        iteration++;
+        if(iteration >= playerSprites.length*duration) iteration = 0;
+    }
+}()
 
 module.exports = {eraseGame: eraseGame, drawGame: drawGame};
 },{}]},{},[3]);
