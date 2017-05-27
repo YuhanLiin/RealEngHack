16c8bdf21ad2b4f46eee0ccf55c4b49ce919af01
yuhan lin <yuhan lin>
2017-05-27 14:42:48 -0400

    Integrated verticle player sprites

--- js/viewEngine.js
+++ js/viewEngine.js
@@ -5,27 +5,32 @@ function drawGame(game){
     drawPlayer(game.player);
 
     ctx.fillStyle='red';
-    ctx.globalAlpha = 0.4;
+    ctx.globalAlpha = 0.2;
     drawHitbox(game.player);
-    ctx.restore();
+    ctx.globalAlpha = 1;
     //Draw other shit
 }
 
 var bg = document.getElementById('bg');
 function eraseGame(game){
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
     ctx.drawImage(bg, 0, 0, canvas.width, canvas.height);
 }
 
-var playerSprites = [document.getElementById('player1'), document.getElementById('player2')];
+var playerSpritesHori = [document.getElementById('player1'), document.getElementById('player2')];
+var playerSpritesVert = [document.getElementById('player3'), document.getElementById('player4')];
 var drawPlayer = function(){
     var iteration = 0;
     var duration = 10
     return function (player){
         //x2 width to compensate for the gun
-        ctx.drawImage(playerSprites[Math.floor(iteration/duration)], player.x-player.width/2, 
+        var sprites;
+        if (player.vely != 0) sprites = playerSpritesVert;
+        else sprites = playerSpritesHori;
+        ctx.drawImage(sprites[Math.floor(iteration/duration)], player.x-player.width/2, 
             player.y-player.height/2, player.width*1.8, player.height);
         if (player.velx != 0 || player.vely != 0) iteration++;
-        if(iteration >= playerSprites.length*duration) iteration = 0;
+        if(iteration >= 2*duration) iteration = 0;
     }
 }();
 
